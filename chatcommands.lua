2dd614ff17f74060a1638254dad56f22390446ab
wsor <wsor@gmail.com>
2021-01-23 20:21:24 -0500

    modify spawnout, add short commands

--- chatcommands.lua
+++ chatcommands.lua
@@ -1,3 +1,21 @@
+------------------
+--short commands--
+------------------
+
+if minetest.get_modpath("areas") then 
+    minetest.register_chatcommand("a1", minetest.chatcommands["area_pos1"])
+    minetest.register_chatcommand("a2", minetest.chatcommands["area_pos2"])
+    minetest.register_chatcommand("p", minetest.chatcommands["protect"])
+end
+
+if minetest.get_modpath("catcommands") then
+    minetest.register_chatcommand("v", minetest.chatcommands["vanish"])
+end
+
+-----------------
+--chat commands--
+-----------------
+
 --perhaps make this load from modstorage?
 local spawnOut = {x=2025,y=7,z=161}
 
@@ -6,9 +24,13 @@ minetest.register_chatcommand("spawnout", {
     description = "adds a command to tp players to spawn out",
     privs = {teleport=true},
     func = function(name,param)
-        if not minetest.get_player_by_name(param) then
+        if param == "" then
+            minetest.chat_send_player(name, "Teleporting " .. name .. " to spawnout")
+            minetest.get_player_by_name(name):set_pos(spawnOut)
+        elseif not minetest.get_player_by_name(param) then
             minetest.chat_send_player(name, "sorry, player ".. param .." does not appear to be ingame and/or exist")
         else
+            minetest.chat_send_player(name, "Teleporting " .. param .. " to spawnout")
             minetest.get_player_by_name(param):set_pos(spawnOut)
         end
     end
