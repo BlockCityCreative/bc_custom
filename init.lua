b3cd8388ffdd7db82ae040793051bfeab0904021
wsor <24964441+wsor4035@users.noreply.github.com>
2021-08-07 15:46:48 -0400

    ptch had issue

--- init.lua
+++ init.lua
@@ -2,3 +2,5 @@ local modpath = minetest.get_modpath("bc_custom")
 dofile(modpath .. "/dev.lua")
 dofile(modpath .. "/rules.lua")
 dofile(modpath .. "/chatcommands.lua")
+
+minetest.override_item('', {wield_image = 'wieldhand.png'})
